e7c8051f9f5d86688ddb1dddef1f78cf70f2888d
kennedy1919 <kennodhiambo433@gmail.com>
2022-03-13 11:54:04 -0700

    task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - entry point
+ *
+ * return: always 0 (Success)
+ */
+int main(void)
+{
+	for (inti = 0; i <= 9; i++)
+	{
+		for(int j = i + 1; j <= 9; j++)
+		{
+			putchar(i + '0')
+			putchar(j + '0')
+			if (((i * 10) + j) < 89)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
