e7283f3d19658665f4ca620710813921fe6b9f72
kennedy1919 <kennodhiambo433@gmail.com>
2022-03-14 06:03:53 -0700

    task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
-
+/**
+ * mai - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	for(int i = 0; i <= 9; i++)
+	int i;
+	for(i = 0; i <= 9; i++)
 	{
 		putchar(i + '0');
 	}
