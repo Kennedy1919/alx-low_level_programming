1e9bdbd95168b1ddcefa2989b9e45e6014c4edf3
Kennedy1919 <kennodhiambo433@gmail.com>
2022-03-18 18:15:42 -0700

    task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_most_numbers - print number
+ *
+ * return: Always 0
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	if (i = 0; i < 10; i++)
+	{
+		if (i != 2 && i != 4)
+		{
+			_putchar(i + '0');
+		}
+	}
+	_putchar('\n');
+}
