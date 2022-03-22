9433952769421d0e5eb7eef941d5842c3e1bb4f8
Kennedy1919 <kennodhiambo433@gmail.com>
2022-03-21 18:11:27 -0700

    git commit -m 'alx tasks'

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * main - entry point
+ *
+ * Return: Void
+ */
+int main(void)
+{
+	int j = 100;
+	int i;
+
+	i = 1;
+	while (i <= j)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			prinf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%i", i);
+		}
+		if (i != 100)
+			printf(' ');
+		i++;
+	}
+	printf('\n');
+	return (0);
+}
