fae609907414d6370a3a982e27ef94e351d6d42c
Kennedy1919 <kennodhiambo433@gmail.com>
2022-04-07 07:10:05 -0700

    task 5

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -5,3 +5,7 @@ char *_memset(char *s, char b, unsigned int n);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 char *_memcpy(char *dest, char *src, unsigned int n);
+void multiply(char* s1, char* s2);
+int _putchar(char c);
+int _strlen(char *);
+int onlyNumbers(char *c);
